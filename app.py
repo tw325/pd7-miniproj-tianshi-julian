c403788c1c3b28af07152f1d2f6489c75bcbd1fd
twangMRwang123 <twang.123@live.com>
2014-10-31 13:16:50 -0400

    templates

--- app.py
+++ app.py
@@ -1,27 +1,28 @@
 from flask import Flask, render_template, request
 
-import utils
 
-app = Flask(_name_)
+app = Flask(__name__)
 
 @app.route("/", methods = ["GET", "POST"])
 @app.route("/home", methods = ["GET","POST"])
 def home():
-    #
+    if request.method=="GET":
+        return render_template("home.html")
 
 @app.route("/login", methods = ["GET","POST"])
 def login():
     if request.method=="POST":
-        return render_template("login.html",
+        return render_template("login.html")
     
 @app.route("/register", methods = ["GET", "POST"])
 def register():
-    #
+    if request.method =="GET":
+        return render_template("register.html")
 
 @app.route("/logout", methods = ["GET"])
 def logout():
-    #
+    pass
 
-if _name_=="_main_":
+if (__name__=='__main__'):
     app.debug=True
     app.run()
